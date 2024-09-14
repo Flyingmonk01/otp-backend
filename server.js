a5fabf6dc0238e0aaec7e10b23b658982051b7d0
Sameer rai <sameerrai350.com>
2024-09-15 00:42:31 +0530

    updated cors

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 import { config } from "dotenv";
 import connectDB from "./db/index.js";
 import app from "./app.js";
+import cors from 'cors';
+
+// Define the whitelist of allowed domains
+const whitelist = ['exp://192.168.21.248:8081']; // Add your allowed domains here
+
+// Configure CORS options to allow only whitelisted domains
+const corsOptions = {
+    origin: function (origin, callback) {
+        // If no origin (like some Postman requests), allow it
+        if (!origin) return callback(null, true);
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true, // If you need to allow cookies
+};
+
+// Use the CORS middleware with the options
+app.use(cors(corsOptions));
 
 config({
     path: ".env",
